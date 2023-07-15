e25be379799123a1713df50969d7931e4d05a079
Naoufal <naoufal.ikkou@gmail.com>
2023-07-15 05:51:51 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,22 +3,17 @@
 
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ * Description: Prints the alphabet in lowercase
  * Return: 0
  */
 
 int main(void) 
 {
-  char alphabet = 'a';
+  char alpha;
 
-  while (alphabet <= 'z')
-  {
-    // print the value of the variable	  
-    putchar(c);
-    // increment the variabble (ie: a becomes b)
-    c++
-  }
+  for(alpha = 'a'; alpaha <= 'z'; alpha++)
+      putchar(alpha);
 
-  putchar('\n');
-  return(0);
+   putchar('\n');
+   return (0);  
 }
