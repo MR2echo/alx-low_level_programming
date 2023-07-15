6b0966fb8d8689a6f8c8015176fca9cf70821e4d
Naoufal <naoufal.ikkou@gmail.com>
2023-07-15 03:46:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
-/*
- * main - prints "Programming is like building a multilingual puzzle,followed by a new line."
- * Return : Always 0.              
- *
- * 
+/**
+ * main - main block
+ * Return: 0
  */
-
-
 int main(void)
 {
-   puts("\"Progamming is like building a multilinual puzzle");
-
-   return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
