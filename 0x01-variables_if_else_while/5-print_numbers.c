7cfb69fb15a243280199e4c00a68cf737c8dd922
Naoufal <naoufal.ikkou@gmail.com>
2023-07-15 06:04:40 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,16 +7,18 @@
  * starting from 0, followed by a new line.
  * Return: 0
  */
+
+
 int main(void)
 {
-     char c = '0';
+	char c = '0';
 
-     while (c <= '9')
-     {
-       putchar(c);
-       c++;
-     }
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
 
-     putchar('\n');
-     return(0);
+	putchar('\n');
+	return (0);
 }
