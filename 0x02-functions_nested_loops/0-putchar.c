a59aaf88b1b98d12ffcebfe4a77b8eefea19698a
Naoufal <naoufal.ikkou@gmail.com>
2023-07-19 03:06:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 
+ * */
+int main(void)
+{
+   _putchar('D');
+   _putchar('R');
+   -putchar('M');
+   _putchar('\n');
+
+   return (0);
+}
