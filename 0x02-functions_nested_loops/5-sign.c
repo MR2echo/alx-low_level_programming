916d97a166f77033c6d77f785ebfb5e69050b11b
Naoufal <naoufal.ikkou@gmail.com>
2023-07-19 04:42:06 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a given number @n
+ * @n: Any given number to be tested and prints '-' or '+' or '0'
+ *
+ * Return: Returns 1 on success
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	return (0);
+}
