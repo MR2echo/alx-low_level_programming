7a7a8c00c14ea7ede80dbc37b66f3cd3c9d1ad8e
Naoufal <naoufal.ikkou@gmail.com>
2023-12-11 06:24:40 -0800

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mod - add of two integers
+ * @a: first unt
+ * @b: second int
+ * Return: final answer
+ *
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul  - multiplay two integers
+ * @a: first unt
+ * @b: second int
+ * Return: multiplay
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
